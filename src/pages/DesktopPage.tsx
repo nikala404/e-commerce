8d66387ab151e98f8ea9e444cd4de72c7476f335
nikala <nikoloz.lursmanashvili.1@btu.edu.ge>
2024-03-22 23:20:13 +0400

    nothing to commit

--- src/pages/DesktopPage.tsx
+++ src/pages/DesktopPage.tsx
@@ -1,26 +1,28 @@
 import Header from "../desktop_page_components/Header";
-import { styled, createGlobalStyle } from "styled-components";
-import Hero from "../desktop_page_components/Hero";
+import { createGlobalStyle } from "styled-components";
+import Hero from "../desktop_page_components/Hero.tsx";
 import MainContent from "../desktop_page_components/MainContent";
+import { useState } from "react";
 
 const GlobalStyle = createGlobalStyle`
-    *{
-        padding:0;
-        margin:0;
-        box-sizing:border-box;
-    }
-    body{
-        padding:20px 20px
-    }
+      *{
+          padding:0;
+          margin:0;
+          box-sizing:border-box;
+      }
+      body{
+          padding:20px 10px
+       }
 
-`;
+  `;
 export default function DesktopPage() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header setSearchQuery={setSearchQuery} />
       <Hero />
-      <MainContent />
+      <MainContent searchQuery={searchQuery} />
     </>
   );
 }
