2a18a7960e777e37142629b7ec6c747991aacc6f
nikala <nikoloz.lursmanashvili.1@btu.edu.ge>
2024-03-22 21:19:27 +0400

    I have added products

--- src/pages/DesktopPage.tsx
+++ src/pages/DesktopPage.tsx
@@ -1,3 +1,26 @@
+import Header from "../desktop_page_components/Header";
+import { styled, createGlobalStyle } from "styled-components";
+import Hero from "../desktop_page_components/Hero";
+import MainContent from "../desktop_page_components/MainContent";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        padding:0;
+        margin:0;
+        box-sizing:border-box;
+    }
+    body{
+        padding:20px 20px
+    }
+
+`;
 export default function DesktopPage() {
-  return <div>DesktopPage</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Hero />
+      <MainContent />
+    </>
+  );
 }
